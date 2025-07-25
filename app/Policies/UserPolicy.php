c27852187fe15146d6fbe3e7474ca17adca9301c
Felipe Di Sessa <felipetmds@hotmail.com>
2025-07-25 10:34:29 -0300

    Cria UserPolicy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+use App\Models\User;
+use App\Enums\UserRole;
+
+
+class UserPolicy
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function isAdmin(User $user): bool
+    {
+        return $user->role === UserRole::ADMIN;
+    }
+}
